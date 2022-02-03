8ab552de07092f6aec31dcdc65cde2ace3567daf
tristiino <73903495+tristiino@users.noreply.github.com>
2022-02-02 22:18:42 -0500

    Initial Commit

---
+++ QuizTrivia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  QuizTrivia
+//
+//  Created by Tristan Barnett on 2/2/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
